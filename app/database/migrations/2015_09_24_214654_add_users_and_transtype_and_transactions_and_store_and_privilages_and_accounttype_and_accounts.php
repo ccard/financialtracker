0cfd0b13934abb79e2aa74e7ba634bd5d1f5bdbc
ccard <ccard@mymail.mines.edu>
2015-09-24 17:25:54 -0500

    got migrations to work using mysql

--- app/database/migrations/2015_09_24_214654_add_users_and_transtype_and_transactions_and_store_and_privilages_and_accounttype_and_accounts.php
+++ app/database/migrations/2015_09_24_214654_add_users_and_transtype_and_transactions_and_store_and_privilages_and_accounttype_and_accounts.php
@@ -25,7 +25,6 @@ class AddUsersAndTranstypeAndTransactionsAndStoreAndPrivilagesAndAccounttypeAndA
 			$table->dateTime('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
 			$table->dateTime('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
 			$table->integer('privilage_id');
-			$table->foreign('privilage_id')->references('id')->on('privilages');
 			$table->string('remember_token')->nullable();
 		});
 		Schema::create('accounttype',function($table){
@@ -36,10 +35,8 @@ class AddUsersAndTranstypeAndTransactionsAndStoreAndPrivilagesAndAccounttypeAndA
 		Schema::create('accounts',function($table){
 			$table->increments('id');
 			$table->integer('user_id');
-			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 			$table->index('user_id');
-			$table->integer('account_type_id')->nullable();
-			$table->foreign('account_type_id')->references('id')->on('accounttype')->onDelete('null');
+			$table->integer('account_type_id');
 			$table->string('institution');
 			$table->double('balance',20,2)->default(0.0);
 			$table->boolean('active')->default(true);
@@ -55,20 +52,16 @@ class AddUsersAndTranstypeAndTransactionsAndStoreAndPrivilagesAndAccounttypeAndA
 		Schema::create('transtype',function($table){
 			$table->increments('id');
 			$table->string('name');
-			$table->boolean('is_credit')->default('false');
+			$table->boolean('is_credit')->default(false);
 		});
 		Schema::create('transactions',function($table){
 			$table->bigIncrements('id');
 			$table->integer('user_id');
-			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 			$table->index('user_id');
-			$table->integer('trans_type_id')->nullable();
-			$table->foreign('trans_type_id')->references('id')->on('transtype')->onDelete('null');
-			$table->integer('store_id')->nullable();
-			$table->foreign('store_id')->references('id')->on('stores')->onDelete('null');
+			$table->integer('trans_type_id');
+			$table->integer('store_id');
 			$table->dateTime('date')->default(DB::raw('CURRENT_TIMESTAMP'));
 			$table->integer('accounts_id');
-			$table->foreign('accounts_id')->references('id')->on('accounts')->onDelete('cascade');
 			$table->index('accounts_id');
 			$table->double('amount',20,2);
 			$table->boolean('posted')->default(false);
